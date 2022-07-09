a560df220ce51c788d0fbd52e6dd335770171863
mystatus987 <101029763+mystatus987@users.noreply.github.com>
2022-07-10 09:47:35 +1000

    add task function

--- App.js
+++ App.js
@@ -1,51 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import TaskItem from './components/TaskItem';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, FlatList, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import { Task } from './components/Task';
+
 export default function App() {
+  // const [task, setTask] = useState();
+  // const [taskItems, setTaskItems] = useState([]);
+
+  // const handleAddTask = () => {
+  //   Keyboard.dismiss();
+  //   setTaskItems([...taskItems, task])
+  //   setTask(null);
+  // }
+
+  // const completeTask = (index) => {
+  //   let itemsCopy = [...taskItems];
+  //   itemsCopy.splice(index, 1);
+  //   setTaskItems(itemsCopy)
+  // }
+
+  // application states array obj
+  const [ListData, SetListData] = useState([])
+  const [input, setInput] = useState('')
+  // reference to text input 
+  // const txtInput = useRef()
+  // function to add value of input to ListData (add an item to list)
+  const addItem = () => {
+    // use timestamp to create unique id
+    let newId = new Date().getTime()
+    let newItem = { id: newId, name: input, status: false }
+    let newList = ListData.concat(newItem)
+    SetListData(newList)
+    // txtInput.clear()
+    // useEffect Hook
+    // useEffect(() => console.log("updating"), [ListData])
+  }
+
+
+  //function to render list item
+  const renderItem = ({ item }) => (
+    <Task item = {item} />
+  )
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container}>  
+        {/* Today's Tasks */}
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Today's tasks</Text>
+          <View style={styles.items}>
 
+            <FlatList
+              data={ListData}
+              keyExtractor={(item) => item.id}
+              renderItem={renderItem}
+            />
 
-      <View style={styles.taskWapper}>
-        <Text style={styles.titleName}>2Do List</Text>
-        <View style= {styles.itmes}>
-            <TaskItem text = {'Task 1'}/>
-            <TaskItem text = {'Task 2'}/>
+          </View>
         </View>
-      </View>
 
-      {/* Write task seccsion */}
-      {/* solve the common problem of views that need to move out of the way of the virtual keyboard. 
-      It can automatically adjust either its height, position, or bottom padding based on the keyboard height. */}
+ 
+  {/* Write a task */}
+      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
       <KeyboardAvoidingView
-        behavior={Platfrom.OS === "ios" ? "padding" : "height"}
-        style = {styles.inputTextWrapper}>
-          <TextInput style={styles.inputText} placeholder={'write you task here'}/>
-        </KeyboardAvoidingView>
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} onChangeText={(value) => setInput(value)}/>
+        <TouchableOpacity onPress={() => addItem()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    
 
     </View>
-
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#49334d',
+    backgroundColor: '#E8EAED',
   },
-  taskWapper:{
+  tasksWrapper: {
     paddingTop: 80,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
-  titleName:{
-    fontSize:24,
-    fontWeight:'bold',
-    color: '#FFF',
-    textAlign: 'center',
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
   },
-  itmes:{
-    marginTop: 30
+  items: {
+    marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
