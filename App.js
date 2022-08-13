65cea695ab89feca889cdcce48ba5d6f653cb455
mystatus987 <101029763+mystatus987@users.noreply.github.com>
2022-08-13 21:19:53 +1000

    rendering is not working

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import { SignupScreen } from "./screens/SignupScreen";
 import { HistoryScreen } from "./screens/HistoryScreen";
 import { SignoutButton } from "./components/SignoutButton";
 import { WelcomeScreen } from "./screens/WelcomeScreen"
+import Storage from "react-native-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 // firebase config
 import { firebaseConfig } from "./config/Config";
 import { initializeApp } from "firebase/app";
@@ -55,16 +57,127 @@ export default function App() {
       })
   }
 
+  const storage = new Storage({
+    // maximum capacity, default 1000 key-ids
+    size: 1000,
+
+    // Use AsyncStorage for RN apps, or window.localStorage for web apps.
+    // If storageBackend is not set, data will be lost after reload.
+    storageBackend: AsyncStorage, // for web: window.localStorage
+
+    // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
+    // can be null, which means never expire.
+    defaultExpires: null,
+
+    // cache data in the memory. default is true.
+    enableCache: true,
+  });
+
+  // application states array obj
+  const [ListData, SetListData] = useState([]);
+  const [starting, setStarting] = useState(true);
+
+  // reference to text input
+  // const txtInput = useRef()
+  // function to add value of input to ListData (add an item to list)
+  const addItem = (input) => {
+    // use timestamp to create unique id
+    console.log(input)
+    let newId = new Date().getTime();
+    let newItem = {
+      id: newId,
+      name: input,
+      status: "1",
+    };
+    let newList = ListData.concat(newItem);
+    newList.filter((item) => {
+      if (item.status == '1') {
+        return SetListData(newList);
+      } else {
+        // check the object 
+        // console.log(newList)
+      }
+    });
+  };
+
+  // storage functions
+  const saveData = () => {
+    storage.save({
+      key: "localListData",
+      data: JSON.stringify(ListData),
+    });
+  };
+  const loadData = () => {
+    storage
+      .load({
+        key: "localListData",
+      })
+      .then((data) => {
+        SetListData(JSON.parse(data));
+      });
+  };
+
+  useEffect(() => {
+    // sortList(ListData);
+    saveData();
+  }, [ListData]);
+
+  useEffect(() => {
+    if (starting) {
+      loadData();
+      setStarting(false);
+    }
+  });
+
+  const completeItem = (itemId) => {
+    const newList = ListData.filter((item) => {
+      if (item.id == itemId) {
+        return item.status = "0";
+      }
+    });
+    SetListData(newList);
+    console.log(newList);
+  }
+
+  const displayComplete = () => {
+    let newList = ListData.concat(newItem);
+    newList.filter((item) => {
+      if (item.status == '0') {
+        return SetListData(newList);
+      } else {
+        // check the object 
+        // console.log(newList)
+      }
+    });
+  }
+
+  const deleteItem = (itemId) => {
+    /*
+    find the item id 
+    remove item with the id from array (ListData)
+    setListData (new array)
+    */
+    const newList = ListData.filter((item) => {
+      if (item.id !== itemId) {
+        return item;
+      }
+    });
+    //  setListData (new array)
+    SetListData(newList);
+    console.log(itemId);
+  };
+  //function to render list item
+  const renderItem = ({ item }) => <Task item={item} remove={deleteItem} complete={completeItem} />;
   return (
     <NavigationContainer>
       <Stack.Navigator>
         {/* to pass additional props we have to change our Stack.screen component */}
         <Stack.Screen name="Hello!" component={WelcomeScreen} ></Stack.Screen>
         <Stack.Screen name="Signup" options={{
-          headerTitleStyle:{
-             fontWeight:'bold',
+          headerTitleStyle: {
+            fontWeight: 'bold',
           },
-          headerBackVisible:false
+          headerBackVisible: false
         }}>
           {(props) => <SignupScreen {...props} signup={register} auth={user} />}
         </Stack.Screen>
@@ -72,12 +185,13 @@ export default function App() {
           {(props) => <SigninScreen {...props} signin={signin} auth={user} />}
         </Stack.Screen>
         <Stack.Screen name="Home" options={{
-           title: "YOUR TASKS",
+          title: "YOUR TASKS",
           headerRight: (props) => <SignoutButton {...props} signout={signout} />
         }}>
-          {(props) => <HomeScreen {...props} auth={user} />}
+          {(props) => <HomeScreen {...props} auth={user} add={addItem} renderItem={renderItem} />}
+
         </Stack.Screen>
-        <Stack.Screen name="History" options={{ headerTitle:"Complete Tasks" }} component={HistoryScreen} />
+        <Stack.Screen name="History" options={{ headerTitle: "Complete Tasks" }} component={HistoryScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,125 +16,125 @@ import { useNavigation } from '@react-navigation/native';
 export function HomeScreen(props) {
 
   const navigation = useNavigation();
-
+  const [input, setInput] = useState("");
   useEffect(() => {
     if (!props.auth) {
       navigation.reset({ index: 0, routes: [{ name: "Signup" }] })
     }
   }, [props.auth])
-  const storage = new Storage({
-    // maximum capacity, default 1000 key-ids
-    size: 1000,
+  // const storage = new Storage({
+  //   // maximum capacity, default 1000 key-ids
+  //   size: 1000,
 
-    // Use AsyncStorage for RN apps, or window.localStorage for web apps.
-    // If storageBackend is not set, data will be lost after reload.
-    storageBackend: AsyncStorage, // for web: window.localStorage
+  //   // Use AsyncStorage for RN apps, or window.localStorage for web apps.
+  //   // If storageBackend is not set, data will be lost after reload.
+  //   storageBackend: AsyncStorage, // for web: window.localStorage
 
-    // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
-    // can be null, which means never expire.
-    defaultExpires: null,
+  //   // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
+  //   // can be null, which means never expire.
+  //   defaultExpires: null,
 
-    // cache data in the memory. default is true.
-    enableCache: true,
-  });
+  //   // cache data in the memory. default is true.
+  //   enableCache: true,
+  // });
 
-  // application states array obj
-  const [ListData, SetListData] = useState([]);
-  const [input, setInput] = useState("");
-  const [starting, setStarting] = useState(true);
+  // // application states array obj
+  // const [ListData, SetListData] = useState([]);
+  // const [input, setInput] = useState("");
+  // const [starting, setStarting] = useState(true);
 
-  // reference to text input
-  // const txtInput = useRef()
-  // function to add value of input to ListData (add an item to list)
-  const addItem = () => {
-    // use timestamp to create unique id
-    let newId = new Date().getTime();
-    let newItem = {
-      id: newId,
-      name: input,
-      status: "1",
-    };
-    let newList = ListData.concat(newItem);
-    newList.filter((item) => {
-      if (item.status == '1') {
-        return SetListData(newList);
-      } else {
-        // check the object 
-        // console.log(newList)
-      }
-    });
-  };
+  // // reference to text input
+  // // const txtInput = useRef()
+  // // function to add value of input to ListData (add an item to list)
+  // const addItem = () => {
+  //   // use timestamp to create unique id
+  //   let newId = new Date().getTime();
+  //   let newItem = {
+  //     id: newId,
+  //     name: input,
+  //     status: "1",
+  //   };
+  //   let newList = ListData.concat(newItem);
+  //   newList.filter((item) => {
+  //     if (item.status == '1') {
+  //       return SetListData(newList);
+  //     } else {
+  //       // check the object 
+  //       // console.log(newList)
+  //     }
+  //   });
+  // };
   
-  // storage functions
-  const saveData = () => {
-    storage.save({
-      key: "localListData",
-      data: JSON.stringify(ListData),
-    });
-  };
-  const loadData = () => {
-    storage
-      .load({
-        key: "localListData",
-      })
-      .then((data) => {
-        SetListData(JSON.parse(data));
-      });
-  };
+  // // storage functions
+  // const saveData = () => {
+  //   storage.save({
+  //     key: "localListData",
+  //     data: JSON.stringify(ListData),
+  //   });
+  // };
+  // const loadData = () => {
+  //   storage
+  //     .load({
+  //       key: "localListData",
+  //     })
+  //     .then((data) => {
+  //       SetListData(JSON.parse(data));
+  //     });
+  // };
 
-  useEffect(() => {
-    // sortList(ListData);
-    saveData();
-  }, [ListData]);
+  // useEffect(() => {
+  //   // sortList(ListData);
+  //   saveData();
+  // }, [ListData]);
 
-  useEffect(() => {
-    if (starting) {
-      loadData();
-      setStarting(false);
-    }
-  });
+  // useEffect(() => {
+  //   if (starting) {
+  //     loadData();
+  //     setStarting(false);
+  //   }
+  // });
 
-  const completeItem = (itemId) => {
-    const newList = ListData.filter((item) => {
-      if (item.id == itemId) {
-        return item.status = "0";
-      }
-    });
-    SetListData(newList);
-    console.log(newList);
-  }
+  // const completeItem = (itemId) => {
+  //   const newList = ListData.filter((item) => {
+  //     if (item.id == itemId) {
+  //       return item.status = "0";
+  //     }
+  //   });
+  //   SetListData(newList);
+  //   console.log(newList);
+  // }
 
-  const displayComplete = () => {
-    let newList = ListData.concat(newItem);
-    newList.filter((item) => {
-      if (item.status == '0') {
-        return SetListData(newList);
-      } else {
-        // check the object 
-        // console.log(newList)
-      }
-    });
-  }
+  // const displayComplete = () => {
+  //   let newList = ListData.concat(newItem);
+  //   newList.filter((item) => {
+  //     if (item.status == '0') {
+  //       return SetListData(newList);
+  //     } else {
+  //       // check the object 
+  //       // console.log(newList)
+  //     }
+  //   });
+  // }
 
-  const deleteItem = (itemId) => {
-    /*
-    find the item id 
-    remove item with the id from array (ListData)
-    setListData (new array)
-    */
-    const newList = ListData.filter((item) => {
-      if (item.id !== itemId) {
-        return item;
-      }
-    });
-    //  setListData (new array)
-    SetListData(newList);
-    console.log(itemId);
-  };
+  // const deleteItem = (itemId) => {
+  //   /*
+  //   find the item id 
+  //   remove item with the id from array (ListData)
+  //   setListData (new array)
+  //   */
+  //   const newList = ListData.filter((item) => {
+  //     if (item.id !== itemId) {
+  //       return item;
+  //     }
+  //   });
+  //   //  setListData (new array)
+  //   SetListData(newList);
+  //   console.log(itemId);
+  // };
 
-  //function to render list item
+  // //function to render list item
   const renderItem = ({ item }) => <Task item={item} remove={deleteItem} complete={completeItem}/>;
-
+  
   return (
     <View style={styles.container}>
       {/* Today's Tasks */}
@@ -145,9 +145,9 @@ export function HomeScreen(props) {
         </TouchableOpacity>
         <View style={styles.items}>
           <FlatList
-            data={ListData}
-            keyExtractor={(item) => item.id}
-            renderItem={renderItem}
+            data={props.ListData}
+            keyExtractor={(props) => props.item.id}
+            renderItem={ props.renderItem}
             ListEmptyComponent={EmptyList}
           />
         </View>
@@ -164,7 +164,7 @@ export function HomeScreen(props) {
           placeholder={"Write a task"}
           onChangeText={(value) => setInput(value)}
         />
-        <TouchableOpacity onPress={() => addItem()}>
+        <TouchableOpacity onPress={() => props.add(input)}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
